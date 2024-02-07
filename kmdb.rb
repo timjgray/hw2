652e36f86f0f3d128f6edae21849c5763a262a33
Tim Gray <tim.gray@kellogg.northwestern.edu>
2024-02-07 12:24:22 +0000

    testing it works

--- kmdb.rb
+++ kmdb.rb
@@ -69,7 +69,8 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+#Model.destory_all
+
 
 # Generate models and tables, according to the domain model.
 # TODO!
